13d5cd97fe0a4baaa7b8368277310dafe998ac83
ChanderG <chandergovind@gmail.com>
2014-07-23 19:20:53 +0530

    Simplified code and base comments

--- README.md
+++ README.md
@@ -3,10 +3,20 @@
 ###What?
 The involves several simple c examples converted to assembly and analyzed to understand x86 (ATAT aka gas method). 
 
+For the safety of the comments, the files starting with c are commented versions of the same file. 
+
+If you build these on a 64 bit machine:
+
 ###Build Instructions
-The C code can be compiled as :
+The C code can be compiled to assembly as :
 ```
-cc -m32 progfile.c
+cc -m32 -S progfile.c
+```
+
+To simplify the assembly code:
+
+```
+cc -m32 -S -fno-asynchronous-unwind-tables progfile.c
 ```
 
 To test the assembly code : 
--- chw.s
+++ chw.s
@@ -1,5 +1,4 @@
-#Simplified assembly code
-  .file	"hw.c"
+	.file	"hw.c"
 	.section	.rodata
 .LC0:
 	.string	"Hello World"
@@ -7,14 +6,24 @@
 	.globl	main
 	.type	main, @function
 main:
+#L implies the label is local to the file 
+.LFB0:                       # Function Begin
+	.cfi_startproc
 	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
 	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
 	andl	$-16, %esp
 	subl	$16, %esp
 	movl	$.LC0, (%esp)
 	call	puts
 	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
 	ret
+	.cfi_endproc
+.LFE0:                      # Function End
 	.size	main, .-main
 	.ident	"GCC: (Ubuntu/Linaro 4.6.3-1ubuntu5) 4.6.3"
 	.section	.note.GNU-stack,"",@progbits
